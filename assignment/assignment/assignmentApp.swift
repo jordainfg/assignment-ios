9ba1b5a37681781c9cc1d6de65a10be461613e53
Jordain <jordainfg@outlook.com>
2024-09-07 15:21:41 +0200

    init

---
+++ assignment/assignment/assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  assignmentApp.swift
+//  assignment
+//
+//  Created by Jordain Gijsbertha on 07/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
